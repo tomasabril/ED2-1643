f3c43105eba912e8820830fc7a4730884df03123
Tomás Abril <tomasabril@users.noreply.github.com>
2015-05-04 16:52:51 -0300

    ler dados

--- main.cpp
+++ main.cpp
@@ -11,8 +11,33 @@
 
 using namespace std;
 
+int d=0;            /// 0 saidas de debug. 1 saida padrao
+
+//variaveis globais
+int n;
+int km[25000];
+int ml[25000];
+int fib[25000];
+
+//funcoes
+void lerdist();
+void calculaFibs();
+void shiftEsq();
+void calculaMilha();
+void print();
+
 int main()
 {
 
     return 0;
+}
+
+void lerdist(){
+    scanf("%d", &n);
+    int tmp;
+    for(int i=0; i<n; i++){
+        scanf("%d", &tmp);
+        km[i] = tmp;
+    }
+    d? :printf("%d n:\n", n);
 }
